8a8668a7b6b99d7f87bf004f0878efbb2e0a77b5
Misba <misbasaba31@gmail.com>
2025-03-01 14:53:49 +0530

    ssh updated

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 console.log('config.js');
 console.log('version 1');
 console.log('new version of config,js');
-console.log('now we are synching changes between local and remote');
+console.log('now we are synching changes between local and remote !!!!');
+console.log("i have added ssh keys");
